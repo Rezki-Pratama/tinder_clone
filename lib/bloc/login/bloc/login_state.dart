68593150fd4ce493c082ec209a11f2d61ef57772
Rezki-Pratama <rezki.pratama7777@gmail.com>
2020-12-15 16:32:54 +0700

    make login state

---
+++ lib/bloc/login/bloc/login_state.dart
@@ -0,0 +1,93 @@
+part of 'login_bloc.dart';
+
+//immutable = all fields should be final
+@immutable
+class LoginState {
+  final bool isEmailValid;
+  final bool isPasswordValid;
+  final bool isSubmitting;
+  final bool isSuccess;
+  final bool isFailure;
+
+  //getter is form valid
+  bool get isFormValid => isEmailValid && isPasswordValid;
+
+  LoginState(
+      {@required this.isEmailValid,
+      @required this.isPasswordValid,
+      @required this.isSubmitting,
+      @required this.isSuccess,
+      @required this.isFailure});
+
+  //initial state / empty
+  factory LoginState.empty() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isSubmitting: false,
+      isFailure: false,
+      isSuccess: false
+    );
+  }
+
+  factory LoginState.loading() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isSubmitting: true,
+      isFailure: false,
+      isSuccess: false
+    );
+  }
+
+  factory LoginState.failure() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isSubmitting: false,
+      isFailure: true,
+      isSuccess: false
+    );
+  }
+
+  factory LoginState.success() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isSubmitting: false,
+      isFailure: false,
+      isSuccess: true
+    );
+  }
+
+  LoginState update({
+    final bool isEmailValid,
+    final bool isPasswordValid
+  }){
+    return copyWith(
+      isEmailValid: isEmailValid,
+      isPasswordValid: isPasswordValid,
+      isSubmitting: false,
+      isFailure: false,
+      isSuccess: true
+    );
+  }
+
+  LoginState copyWith({
+    bool isEmailValid,
+    bool isPasswordValid,
+    bool isSubmitting,
+    bool isFailure,
+    bool isSuccess
+  }){
+    return LoginState(
+      //null coalescing operator  ex: 'abc' ?? ''
+      isEmailValid: isEmailValid ?? this.isEmailValid,
+      isPasswordValid: isPasswordValid ?? this.isPasswordValid,
+      isSubmitting: isSubmitting ?? this.isSubmitting,
+      isSuccess: isSuccess ?? this.isSuccess,
+      isFailure: isFailure ?? this.isFailure
+    );
+  }
+
+}
