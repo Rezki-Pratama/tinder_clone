587f49819e656b32b65ab7a76e7972d3ae528781
Rezki-Pratama <rezki.pratama7777@gmail.com>
2021-01-02 17:04:47 +0700

    add profile page

---
+++ lib/ui/pages/profile.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tinder/bloc/profile/bloc/profile_bloc.dart';
+import 'package:tinder/repositories/user_repositories.dart';
+import 'package:tinder/ui/widgets/profile_form.dart';
+
+import '../utilities.dart';
+
+class Profile extends StatelessWidget {
+  final _userRepository;
+  final userId;
+
+  Profile({@required UserRepository userRepository, String userId})
+      : assert(userRepository != null && userId != null),
+        _userRepository = userRepository,
+        userId = userId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile Setup"),
+        centerTitle: true,
+        backgroundColor: backgroundColor,
+        elevation: 0,
+      ),
+      body: BlocProvider<ProfileBloc>(
+        create: (context) => ProfileBloc(_userRepository),
+        child: ProfileForm(
+          userRepository: _userRepository,
+        ),
+      ),
+    );
+  }
+}
