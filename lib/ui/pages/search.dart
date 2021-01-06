151918601ff74c41727d487ab42d016e8bd25466
Rezki-Pratama <rezki.pratama7777@gmail.com>
2021-01-06 09:35:42 +0700

    get different location with Geolocator

--- lib/ui/pages/search.dart
+++ lib/ui/pages/search.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:tinder/bloc/search/bloc/search_bloc.dart';
+import 'package:tinder/model/user.dart';
+import 'package:tinder/repositories/search_repositories.dart';
 import 'package:tinder/ui/utilities.dart';
 
 class Search extends StatefulWidget {
@@ -10,6 +15,21 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+
+  final SearchRepository _searchRepository = SearchRepository();
+  SearchBloc _searchBloc;
+  User _user, _currentUser;
+  int difference;
+
+  getDifference(GeoPoint userLocation) async {
+    Position position = await Geolocator.getCurrentPosition();
+
+    double location = Geolocator.distanceBetween(userLocation.latitude,
+        userLocation.longitude, position.latitude, position.longitude);
+
+    difference = location.toInt();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
